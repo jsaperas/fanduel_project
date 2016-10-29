c3949452ee65eb9cfe0975488021cc187ec0f507
jcma08 <jcma08@gmail.com>
2016-10-29 10:38:09 -0400

    started on database

--- pull_historical_data.py
+++ pull_historical_data.py
@@ -3,11 +3,24 @@
 # In the future we want to collect injury reports 
 # Also want to collect more data 
 
+import requests
+import os
+import string
 
-url='http://www.basketball-reference.com/teams/'
+dir='c://Users/James//Desktop//fanduel_project'
+os.chdir(dir)
 
 
+
+########################################################################################################
 # params
+
+# Loop a-z
+alphabet=string.lowercase
+
+url='http://www.basketball-reference.com/players/{alpha}/'.format(alpha=alphabet[0])
+
+
 years=[
 		'2017', # current year
 		'2016',
@@ -53,6 +66,16 @@ west=[
 ]
 
 
+class data_container:
+	'''
+	I think this might have a few parts to it:
+	1. Iterate all players in the date ranges, filter ones that dont fall in date range.
+	2. For each player pull all of their historical data.
+	3. For the update, check for the most recent record, should be today's date, and pull onlly that.
+	'''
+	def __init__(self):
+		pass
+	
 
 # update data
 
