b737ac9b52664ccd2af3849b4a66bf211d8a5177
jcma08 <jcma08@gmail.com>
2016-10-29 10:23:23 -0400

    first commit!!

---
+++ pull_historical_data.py
@@ -0,0 +1,58 @@
+
+# Collect historical data
+# In the future we want to collect injury reports 
+# Also want to collect more data 
+
+
+url='http://www.basketball-reference.com/teams/'
+
+
+# params
+years=[
+		'2017', # current year
+		'2016',
+		'2015',
+		'2014',
+		'2013'
+]
+
+east=[
+		'ATL',
+		'CHI',
+		'CHO', 
+		'CLE', 
+		'TOR', 
+		'MIA', 
+		'IND', 
+		'DET', 
+		'BOS', 
+		'BRK', 
+		'WAS', 
+		'PHI', 
+		'ORL', 
+		'NYK', 
+		'MIL' 
+]
+
+west=[
+		'MEM', 
+		'SAS', 
+		'DEN', 
+		'OKC', 
+		'LAC', 
+		'HOU', 
+		'UTA', 
+		'GSW', 
+		'LAL', 
+		'SAC', 
+		'POR', 
+		'PHO', 
+		'MIN', 
+		'DAL', 
+		'NOP' 
+]
+
+
+
+# update data
+
