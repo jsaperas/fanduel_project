85d5d5a57bd6e3e7cc61556c6de06688de8027d7
jcma08 <jcma08@gmail.com>
2016-11-19 16:15:43 -0500

    added transformation, feature creation, need to add opposing team stats

--- data_transformation/1_Transform.R
+++ data_transformation/1_Transform.R
@@ -17,10 +17,31 @@ dataset=dataset[pts!='PTS']
 require(zoo)
 
 dataset=dataset[order(date_game,descending=T)]
+
+
+# fix data types
 dataset$pts=dataset$pts=ifelse(dataset$pts=='','0',dataset$pts)
+dataset$pts=as.numeric(dataset$pts)
+
+
+dataset$trb=dataset$trb=ifelse(dataset$trb=='','0',dataset$trb)
+dataset$trb=as.numeric(dataset$trb)
+
+dataset$blk=dataset$blk=ifelse(dataset$blk=='','0',dataset$blk)
+dataset$blk=as.numeric(dataset$blk)
+
+dataset$ast=dataset$ast=ifelse(dataset$ast=='','0',dataset$ast)
+dataset$ast=as.numeric(dataset$ast)
+
+dataset$tov=dataset$tov=ifelse(dataset$tov=='','0',dataset$tov)
+dataset$tov=as.numeric(dataset$tov)
+
+dataset$stl=dataset$stl=ifelse(dataset$stl=='','0',dataset$stl)
+dataset$stl=as.numeric(dataset$stl)
 
 
 
+# create features
 shifter=function(x,k=6){
   n=length(x)
   x=rollmean(as.numeric(x),align='right',k=min(NROW(x),k),fill=NA)
@@ -33,10 +54,24 @@ shifter=function(x,k=6){
     return(vec)
   }
 }
+
+
 dataset[,pts_roll_three:=shifter(pts,k=6),by=(player_name)]
-dataset[,three_mean:=shifter(pts),by=(player_name)]
+dataset[,trb_roll_three:=shifter(pts,k=6),by=(player_name)]
+dataset[,ast_roll_three:=shifter(pts,k=6),by=(player_name)]
+dataset[,tov_roll_three:=shifter(pts,k=6),by=(player_name)]
+dataset[,blk_roll_three:=shifter(pts,k=6),by=(player_name)]
+dataset[,stl_roll_three:=shifter(pts,k=6),by=(player_name)]
 
 
+# response
+dataset[,total_fd_pts:=1.0*pts+
+                       1.2*trb+
+                       1.5*ast+
+                       2.0*stl+
+                       2.0*blk+
+                      -1.0*tov
+]
 
 
 #dataset[,three_mean:=rollapplyr(as.numeric(pts),list(-(min(NROW(pts),6):1)),mean,fill=NA),by=(player_name)]
@@ -44,9 +79,6 @@ dataset[,three_mean:=shifter(pts),by=(player_name)]
 dataset[player_name=='Solomon Hill']
 
 
-
-
-# create response variable
 
 
 
